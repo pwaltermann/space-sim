14c8951ad20b82237ea4f5d6ffe269ce576820be
pwaltermann <philipp.waltermann@rwth-aachen.de>
2025-05-07 13:03:46 +0200

    Create run.py

---
+++ run.py
@@ -0,0 +1,16 @@
+import subprocess
+import sys
+
+# Start all processes
+main_process = subprocess.Popen([sys.executable, "main.py"])
+agent1_process = subprocess.Popen([sys.executable, "dummy_agents/stupid_agent.py"])
+agent2_process = subprocess.Popen([sys.executable, "dummy_agents/spinning_agent.py"])
+
+try:
+    main_process.wait()
+except KeyboardInterrupt:
+    print("\nShutting down...")
+finally:
+    main_process.terminate()
+    agent1_process.terminate()
+    agent2_process.terminate() 
